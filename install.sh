7bfd50833dd13246b4b476dc172290d52b000a60
Neop <neop@gmail.com>
2023-11-08 18:58:21 +1300

    apps updated.

--- install.sh
+++ install.sh
@@ -25,3 +25,46 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 sudo flatpak install flathub com.visualstudio.code -y
 # Flathub install Microsoft Edge
 sudo flatpak install flathub com.microsoft.Edge -y
+
+# Install Grub Optimizer
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
+sudo apt install grub-customizer -y
+
+# Install Gnome Tweaks
+sudo apt install gnome-tweaks -y
+
+# Install Gnome Extensions
+sudo apt install gnome-shell-extensions -y
+
+# Install Gparted
+sudo apt install gparted -y
+
+# Install Neofetch
+sudo apt install neofetch -y
+
+# Install Htop
+sudo apt install htop -y
+
+# Install zsh
+sudo apt install zsh -y
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Thunar
+sudo apt install thunar -y
+
+# Install Thunar Plugins
+sudo apt install thunar-archive-plugin -y
+
+# Install Thunar Media Tags
+sudo apt install thunar-media-tags-plugin -y
+
+# Install Thunar Thumbnailers
+sudo apt install ffmpegthumbnailer -y
+
+# Install Thunar Custom Actions
+sudo apt install thunar-custom-actions -y
+
+# Install Thunar Send To
+sudo apt install thunar-sendto-clamtk -y
