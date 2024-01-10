719449169331649afbf8385a1f8632c984dc8c6f
neotubu <aben26@gmail.com>
2024-01-11 11:23:55 +1300

    Updated Xmind Flatak install

--- quickinstall.sh
+++ quickinstall.sh
@@ -12,7 +12,7 @@ sudo apt install snapd -y
 sudo snap install spotify
 sudo snap install chromium
 
-# Install Flatpak
+# Install Flatpak and Flatpak Apps
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -20,6 +20,8 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 sudo flatpak install flathub com.visualstudio.code -y
 # Flathub install Microsoft Edge
 sudo flatpak install flathub com.microsoft.Edge -y
+# Flathub install Xmind
+sudo flatpak install flathub net.xmind.XMind -y
 
 # Installing Twingate
 curl -s https://binaries.twingate.com/client/linux/install.sh | sudo bash
