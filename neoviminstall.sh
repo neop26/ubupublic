a89776cb0e2a4eb30ccbaf1a6f9f54d47caa039a
neotubu <aben26@gmail.com>
2024-02-08 10:14:59 +1300

    added neovim installer script

---
+++ neoviminstall.sh
@@ -0,0 +1,7 @@
+  sudo add-apt-repository ppa:neovim-ppa/unstable -y
+  sudo apt-get update -y
+  sudo apt install build-essential -y
+  sudo apt-get install manpages-dev -y
+  sudo apt install cmake -y
+  sudo apt-get install ninja-build gettext libtool-bin cmake g++ pkg-config unzip curl -y
+  sudo apt-get install neovim -y
