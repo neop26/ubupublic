6675b4e4f98e02c609fb6d7ef550d6feea9438a5
neotubu <aben26@gmail.com>
2024-02-08 09:45:05 +1300

    ctsetup.sh added

---
+++ ctsetup.sh
@@ -0,0 +1,15 @@
+# Bash script to install applications for a fresh Ubuntu CT
+
+#!/bin/bash
+
+# Update and Upgrade
+sudo apt update && sudo apt upgrade -y
+
+# Add Net Tools
+sudo apt install net-tools -y
+
+# Install Neofetch
+sudo apt install neofetch -y
+
+# Install Htop
+sudo apt install htop -y
