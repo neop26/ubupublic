723bea1d7a9f232c78c8c7eeed7a7ed656ce45fd
Aben Samuel <neo@ubucont1.neop26.io>
2024-02-07 21:39:16 +0000

    small update to neovim installer script

--- neoviminstall.sh
+++ neoviminstall.sh
@@ -1,12 +1,12 @@
-  sudo add-apt-repository ppa:neovim-ppa/unstable -y
-  sudo apt-get update -y
-  sudo apt install build-essential -y
-  sudo apt-get install manpages-dev -y
-  sudo apt install cmake -y
-  sudo apt-get install ninja-build gettext libtool-bin cmake g++ pkg-config unzip curl -y
-  sudo apt-get install neovim -y
-  # Configuring LazyVim
-  sudo mkdir .config/nvim
-  sudo git clone https://github.com/LazyVim/starter ~/.config/nvim
-  sudo cd ~/.config/nvim
-  sudo nvim
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt-get update -y
+sudo apt install build-essential -y
+sudo apt-get install manpages-dev -y
+sudo apt install cmake -y
+sudo apt-get install ninja-build gettext libtool-bin cmake g++ pkg-config unzip curl -y
+sudo apt-get install neovim -y
+# Configuring LazyVim
+sudo mkdir .config/nvim
+sudo git clone https://github.com/LazyVim/starter ~/.config/nvim
+# cd ~/.config/nvim
+sudo nvim
