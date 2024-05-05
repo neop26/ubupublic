8e442c2980d01559c0fc2871bc77598e30fc1fb8
Aben Samuel <aben26@gmail.com>
2024-05-06 02:09:41 +1200

    chore: Update server template and installation scripts

---
+++ install-scripts/update.sh
@@ -0,0 +1,11 @@
+# Bash script to Update the system
+
+#!/bin/bash
+
+# Update the package list
+sudo apt update
+
+echo "System updated successfully"
+wait 2
+
+clear
--- ubuservertemplate.sh
+++ ubuservertemplate.sh
@@ -24,24 +24,105 @@ echo " \/_/     \/_/ \/__________/\/_________/       "
 printf "\n%.0s" {1..2}                                      
 
 
-# Update and Upgrade
-sudo apt update && sudo apt upgrade -y
+# Create Directory for Install Logs
+if [ ! -d Install-Logs ]; then
+    mkdir Install-Logs
+fi
 
-# Add Net Tools
-sudo apt install net-tools -y
+# Set some colors for output messages
+OK="$(tput setaf 2)[OK]$(tput sgr0)"
+ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
+NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
+WARN="$(tput setaf 166)[WARN]$(tput sgr0)"
+CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
+ORANGE=$(tput setaf 166)
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr0)
 
-# Install Neofetch
-sudo apt install neofetch -y
+# Function to colorize prompts
+colorize_prompt() {
+    local color="$1"
+    local message="$2"
+    echo -n "${color}${message}$(tput sgr0)"
+}
 
-# Install Htop
-sudo apt install htop -y
+# Set the name of the log file to include the current date and time
+LOG="install-$(date +%d-%H%M%S).log"
 
-# Ability to deploy repository
-sudo apt install --reinstall software-properties-common -y
+# Define the directory where your scripts are located
+script_directory=install-scripts
 
-# Install oh-my-zsh
-sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Oh-My-Zsh has been installed!"
+# Function to ask a yes/no question and set the response in a variable
+ask_yes_no() {
+    while true; do
+        read -p "$(colorize_prompt "$CAT"  "$1 (y/n): ")" choice
+        case "$choice" in
+            [Yy]* ) eval "$2='Y'"; return 0;;
+            [Nn]* ) eval "$2='N'"; return 1;;
+            * ) echo "Please answer with y or n.";;
+        esac
+    done
+}
 
-echo "All done!"
+# Function to ask a custom question with specific options and set the response in a variable
+ask_custom_option() {
+    local prompt="$1"
+    local valid_options="$2"
+    local response_var="$3"
+
+    while true; do
+        read -p "$(colorize_prompt "$CAT"  "$prompt ($valid_options): ")" choice
+        if [[ " $valid_options " == *" $choice "* ]]; then
+            eval "$response_var='$choice'"
+            return 0
+        else
+            echo "Please choose one of the provided options: $valid_options"
+        fi
+    done
+}
+# Function to execute a script if it exists and make it executable
+execute_script() {
+    local script="$1"
+    local script_path="$script_directory/$script"
+    if [ -f "$script_path" ]; then
+        chmod +x "$script_path"
+        if [ -x "$script_path" ]; then
+            "$script_path"
+        else
+            echo "Failed to make script '$script' executable."
+        fi
+    else
+        echo "Script '$script' not found in '$script_directory'."
+    fi
+}
+
+# Collect user responses to all questions
+printf "\n"
+ask_yes_no "-Shall I update this system" update
+printf "\n"
+
+### Update and Upgrade
+##sudo apt update && sudo apt upgrade -y
+##
+### Add Net Tools
+##sudo apt install net-tools -y
+##
+### Install Neofetch
+##sudo apt install neofetch -y
+##
+### Install Htop
+##sudo apt install htop -y
+##
+### Ability to deploy repository
+##sudo apt install --reinstall software-properties-common -y
+##
+### Install oh-my-zsh
+##sudo apt install zsh -y
+##sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+##echo "Oh-My-Zsh has been installed!"
+##
+##echo "All done!"
+
+if [ "$update" == "Y" ]; then
+    execute_script "update.sh"
+fi
