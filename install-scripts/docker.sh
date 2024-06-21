f1dd5fc5d598a6d5188bd40c632e7fd9ad64dd57
Aben Samuel <aben26@gmail.com>
2024-06-22 06:39:47 +1200

    chore: Update Docker installation script to include -y flag for apt commands

--- install-scripts/docker.sh
+++ install-scripts/docker.sh
@@ -1,6 +1,6 @@
 # Installing Docker on the Server
 
-sudo apt install docker.io && sudo apt install docker-compose
+sudo apt install docker.io -y && sudo apt install docker-compose -y
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -aG docker $USER
